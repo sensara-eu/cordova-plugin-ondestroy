bf60742c916d8f40fd71385d4db1a77ee2587193
Peter Xhofleer <peter.xhofleer@luminis.eu>
2019-03-26 10:26:02 +0100

    LPC-205: Added the notification also on the onTaskRemoved method

--- src/android/OnDestroyCheckerService.java
+++ src/android/OnDestroyCheckerService.java
@@ -3,10 +3,11 @@ package com.flyingsoftgames.ondestroyplugin;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class OnDestroyCheckerService extends Service {
 
-  private static final String TAG = "onDestroyChecker";
+  private static final String LOG_TAG = "onDestroyChecker";
 
   @Override
   public IBinder onBind(Intent intent) {
@@ -16,13 +17,27 @@ public class OnDestroyCheckerService extends Service {
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
     super.onStartCommand(intent, flags, startId);
+    Log.d(LOG_TAG, "onStartCommand");
     NotificationHelper.cancelNotification(getApplicationContext());
     return START_NOT_STICKY;
   }
 
+  /*
+   * Needed onTaskRemoved and onDestroy some android api's onTaskRemoved is called and some the
+   * onDestroy
+   */
+
+  @Override
+  public void onTaskRemoved(Intent rootIntent) {
+    super.onTaskRemoved(rootIntent);
+    Log.d(LOG_TAG, "onTaskRemoved");
+    NotificationHelper.createNotification(getApplicationContext());
+  }
+
   @Override
   public void onDestroy() {
-    NotificationHelper.createNotification(getApplicationContext());
     super.onDestroy();
+    Log.d(LOG_TAG, "onDestroy");
+    NotificationHelper.createNotification(getApplicationContext());
   }
 }
